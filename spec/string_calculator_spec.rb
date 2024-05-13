29683487c63262cf4481f363fbd5084ca7e0764f
jay <jay.thakur.sdb@gmail.com>
2024-05-13 13:37:31 +0530

    Added StringCalculator class with add method

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,15 @@
+# spec/string_calculator_spec.rb
+
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+
+    # More test cases will be added here as we progress with the development.
+  end
+end
---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+# string_calculator.rb
+
+class StringCalculator
+  def self.add(numbers)
+    # implementation will go here
+  end
+end
