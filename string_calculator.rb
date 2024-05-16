7bca805beef64f1f2414c27d7cef9e17de238138
jay <jay.thakur.sdb@gmail.com>
2024-05-17 00:30:59 +0530

    Added logic to fix failed test cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,24 @@
 
 class StringCalculator
   def self.add(numbers)
-    numbers.empty? ? 0 : numbers.split(',').map(&:to_i).reduce(:+)
+    # Return 0 if the input string is empty
+    return 0 if numbers.empty?
+
+    # Default delimiter is ','
+    delimiter = ','
+
+    # Check if the input string starts with a custom delimiter definition
+    if numbers.start_with?('//')
+      # Extract the delimiter and numbers part from the input string
+      delimiter, numbers = numbers.split("\n", 2)
+      # Remove the '//' prefix to get the actual delimiter
+      delimiter = delimiter[2..-1]
+    end
+
+    # Split the numbers string using the delimiter (and newline character)
+    nums = numbers.split(/[\n#{delimiter}]/).map(&:to_i)
+
+    # Return the sum of the numbers
+    nums.reduce(:+)
   end
 end
